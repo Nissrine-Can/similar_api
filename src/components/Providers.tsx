57ce9d14e2531ce3934c9360ca3fea622ac056c3
Nissrine Canina <niss.canina@gmail.com>
2023-04-28 21:27:24 -0400

    added theme & session providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,12 @@
 'use client'
-
+import { ThemeProvider }from 'next-themes'
 import { FC, ReactNode } from 'react'
-
+import { SessionProvider } from 'next-auth/react'
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <div>Providers</div>
+  return <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+           <SessionProvider>{children}</SessionProvider>
+         </ThemeProvider>
 }
 
 export default Providers
