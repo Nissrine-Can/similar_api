01e1a320d37c703faad8b37e71e617bde9211dd2
Nissrine Canina <niss.canina@gmail.com>
2023-05-04 20:42:18 -0400

    added sign in component

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,6 +2,7 @@
 
 import { FC, useState } from 'react'
 import Button from '@/ui/Button'
+import { signIn } from 'next-auth/react'
 
 interface SignInButtonProps {
   
@@ -10,7 +11,25 @@ interface SignInButtonProps {
 const SignInButton: FC<SignInButtonProps> = ({}) => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  return <Button>Sign in</Button>
+    const signInWithGoogle = async () => {
+        setIsLoading(true)
+
+        try {
+           await signIn('google')
+        } catch (error) {
+        //    toast({
+        //     title: 'Error signing in',
+        //     message: 'Please try again later',
+        //     type: 'error',
+        //    })
+        }
+    }
+
+  return (
+    <Button onClick={signInWithGoogle} isLoading={isLoading}>
+        Sign in
+    </Button>
+  )
 }
 
 export default SignInButton
